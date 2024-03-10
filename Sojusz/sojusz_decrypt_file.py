df92bed7a89b09d5eeec10db4005de39f9f45790
Andrey Zhdanov <56387892+rivitna@users.noreply.github.com>
2024-03-10 19:03:22 +0300

    Update sojusz_decrypt_file.py

--- Sojusz/sojusz_decrypt_file.py
+++ Sojusz/sojusz_decrypt_file.py
@@ -26,11 +26,7 @@ import sys
 import io
 import os
 import shutil
-import struct
-import math
 import binascii
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_v1_5
 import salsa
 
 
@@ -40,11 +36,15 @@ RANSOM_EXT_PREFIX = '.['
 RANSOM_EXT_POSTFIX = ']'
 
 
+ID_SIZE = 10
+
 # RSA
 RSA_KEY_SIZE = 256
 RSA_KEY_HEX_SIZE = 2 * RSA_KEY_SIZE
 
 # Salsa20
+SALSA_KEY_SIZE = 32
+SALSA_KEY_HEX_SIZE = 2 * 32
 SALSA_NONCE_SIZE = 8
 SALSA_ROUNDS = 8
 
@@ -53,9 +53,6 @@ METADATA_SIZE = SALSA_NONCE_SIZE + RSA_KEY_HEX_SIZE
 
 
 ENC_BLOCK_SIZE = 0x19000
-
-
-SENTINEL_SIZE = 16
 
 
 def load_str_list(filename):
@@ -80,64 +77,10 @@ def is_important_file_ext(filename: str, blackexts: list) -> bool:
     return False
 
 
-def rsa_construct_blob(blob: bytes) -> RSA.RsaKey:
-    """Construct RSA key from BLOB"""
-
-    is_private = False
-
-    type_ver, key_alg, magic, key_bitlen = struct.unpack_from('<4L', blob, 0)
-    # "RSA2"
-    if (type_ver == 0x207) and (key_alg == 0xA400) and (magic == 0x32415352):
-        is_private = True
-    # "RSA1"
-    elif (type_ver != 0x206) or (key_alg != 0xA400) or (magic != 0x31415352):
-        raise ValueError('Invalid RSA blob')
-
-    pos = 16
-    key_len = math.ceil(key_bitlen / 8)
-
-    e = int.from_bytes(blob[pos : pos + 4], byteorder='little')
-    pos += 4
-    n = int.from_bytes(blob[pos : pos + key_len], byteorder='little')
-
-    if not is_private:
-        return RSA.construct((n, e))
-
-    key_len2 = math.ceil(key_bitlen / 16)
-
-    pos += key_len
-    p = int.from_bytes(blob[pos : pos + key_len2], byteorder='little')
-    pos += key_len2
-    q = int.from_bytes(blob[pos : pos + key_len2], byteorder='little')
-    pos += key_len2
-    dp = int.from_bytes(blob[pos : pos + key_len2], byteorder='little')
-    pos += key_len2
-    dq = int.from_bytes(blob[pos : pos + key_len2], byteorder='little')
-    pos += key_len2
-    iq = int.from_bytes(blob[pos : pos + key_len2], byteorder='little')
-    pos += key_len2
-    d = int.from_bytes(blob[pos : pos + key_len], byteorder='little')
-
-    if (dp != d % (p - 1)) or (dq != d % (q - 1)):
-        raise ValueError('Invalid RSA blob')
-
-    return RSA.construct((n, e, d, p, q))
-
-
-def rsa_decrypt(enc_data: bytes, priv_key: RSA.RsaKey) -> bytes:
-    """RSA decrypt data"""
-
-    sentinel = os.urandom(SENTINEL_SIZE)
-    cipher = PKCS1_v1_5.new(priv_key)
-    data = cipher.decrypt(enc_data[::-1], sentinel)
-    if data == sentinel:
-        return None
-    return data
-
-
 def decrypt_file(filename: str,
                  is_important_file: bool,
-                 priv_key: RSA.RsaKey) -> bool:
+                 key: bytes,
+                 enc_key_hex_data: bytes) -> bool:
     """Decrypt file"""
 
     with io.open(filename, 'rb+') as f:
@@ -150,16 +93,15 @@ def decrypt_file(filename: str,
 
         metadata = f.read(METADATA_SIZE)
 
-        # Decrypt Salsa20 key
-        enc_key = binascii.unhexlify(metadata[SALSA_NONCE_SIZE:])
-        key = rsa_decrypt(enc_key, priv_key)
-        if not key:
+        # Check encrypted key data
+        if metadata[SALSA_NONCE_SIZE:] != enc_key_hex_data:
             return False
 
         # Remove metadata
         f.seek(-METADATA_SIZE, 2)
         f.truncate()
 
+        # Decrypt Salsa20 key
         nonce = metadata[:SALSA_NONCE_SIZE]
         cipher = salsa.Salsa(salsa.Salsa.init_state(key, nonce),
                              SALSA_ROUNDS)
@@ -186,38 +128,32 @@ def decrypt_file(filename: str,
 #
 # Main
 #
-if len(sys.argv) != 2:
-    print('Usage:', os.path.basename(sys.argv[0]), 'filename')
+if not (2 <= len(sys.argv) <= 3):
+    print('Usage:', os.path.basename(sys.argv[0]), 'filename [keyname]')
     sys.exit(0)
 
 filename = sys.argv[1]
 
-# Read master/session RSA private key BLOB
-with io.open('./privkey.txt', 'rb') as f:
-    priv_key_blob = binascii.unhexlify(f.read())
-
-# Get RSA private key from BLOB
-priv_key = rsa_construct_blob(priv_key_blob)
-if (priv_key is None) or not priv_key.has_private():
-    print('Error: Invalid RSA private key BLOB')
-    sys.exit(1)
+key_filename = None
+if len(sys.argv) > 2:
+    key_filename = sys.argv[2]
 
 # Load extension blacklist
 blackexts = load_str_list('./blackexts.txt')
-print(len(blackexts))
 
-# Copy file
 new_filename = None
+enc_id = None
 
-# Get original file name
+# Parse encrypted file name
 if filename.endswith(RANSOM_EXT_POSTFIX + RANSOM_EXT):
     pos = len(filename) - (len(RANSOM_EXT_POSTFIX) + len(RANSOM_EXT))
-    for i in range(2):
-        pos = filename.rfind(RANSOM_EXT_PREFIX, 0, pos)
-        if pos < 0:
-            break
-    if pos >= 0:
-        new_filename = filename[:pos]
+    pos = filename.rfind(RANSOM_EXT_POSTFIX + RANSOM_EXT_PREFIX, 0, pos)
+    if pos > ID_SIZE + len(RANSOM_EXT_PREFIX):
+        pos -= ID_SIZE + len(RANSOM_EXT_PREFIX)
+        if filename[pos : pos + 2] == RANSOM_EXT_PREFIX:
+            enc_id = filename[pos + 2 : pos + 2 + ID_SIZE]
+            print('ID:', enc_id)
+            new_filename = filename[:pos]
 
 is_file_important = \
     is_important_file_ext(new_filename if new_filename else filename,
@@ -226,10 +162,26 @@ is_file_important = \
 if not new_filename:
     new_filename = filename + '.dec'
 
+if not key_filename:
+    # Get key file name from ID
+    if not enc_id:
+        print('Error: Specify key file')
+        sys.exit(1)
+    key_filename = os.path.join(os.path.abspath(os.path.dirname(filename)),
+                                enc_id + '.key')
+
+# Read encryption key data
+with io.open(key_filename, 'rb') as f:
+    key_hex_data = f.read(SALSA_KEY_HEX_SIZE)
+    enc_key_hex_data = f.read(RSA_KEY_HEX_SIZE)
+
+key = binascii.unhexlify(key_hex_data)
+
+# Copy file
 shutil.copy(filename, new_filename)
 
 # Decrypt file
-if not decrypt_file(new_filename, is_file_important, priv_key):
+if not decrypt_file(new_filename, is_file_important, key, enc_key_hex_data):
     os.remove(new_filename)
     print('Error: Failed to decrypt file')
     sys.exit(1)
