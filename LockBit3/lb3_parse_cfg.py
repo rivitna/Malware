0fe4e15f500e06df9f874911fdca05f759bd1599
Andrey Zhdanov <56387892+rivitna@users.noreply.github.com>
2024-07-22 00:39:15 +0300

    Update lb3_parse_cfg.py

--- LockBit3/lb3_parse_cfg.py
+++ LockBit3/lb3_parse_cfg.py
@@ -249,7 +249,7 @@ def save_data_to_file(file_name, data):
 # Main
 #
 if len(sys.argv) != 2:
-    print('Usage: '+ sys.argv[0] + ' filename')
+    print('Usage:', os.path.basename(sys.argv[0]), 'filename')
     sys.exit(0)
 
 file_name = sys.argv[1]
